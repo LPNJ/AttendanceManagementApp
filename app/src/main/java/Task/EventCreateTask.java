b744ddab79bdedb9cf9823d63692a6ef566b4cfd
LPNJ <Hiroto.Komiyama@jp.ricoh.com>
2019-12-03 20:08:43 +0900

    （どういう変更をしたか）インターフェースを生成（TASK）しました
    （なぜその変更をしたのか）

--- app/src/main/java/Task/EventCreateTask.java
+++ app/src/main/java/Task/EventCreateTask.java
@@ -1,13 +1,14 @@
 package Task;
 
-import entity.EditableUserInfo;
+import entity.EventInfo;
 
-public interface RegisterTask {
+public interface EventCreateTask {
+
     /**
      * メインスレッドとは異なる非同期処理（通信など）を実施し、ResultListener に対して
      * 実行結果を渡す処理。
-     * @param userInfo ログインに使うユーザー情報
+     * @param eventInfo ログインに使うユーザー情報
      * @param resultListener ログインタスクの結果を受けて処理を実行するリスナー（画面）
      */
-    void execute(EditableUserInfo userInfo, ResultListener resultListener);
+    void execute(EventInfo eventInfo, ResultListener resultListener);
 }
--- app/src/main/java/Task/EventSelectTask.java
+++ app/src/main/java/Task/EventSelectTask.java
@@ -2,7 +2,8 @@ package Task;
 
 import entity.EditableUserInfo;
 
-public interface RegisterTask {
+public interface EventSelectTask {
+
     /**
      * メインスレッドとは異なる非同期処理（通信など）を実施し、ResultListener に対して
      * 実行結果を渡す処理。
@@ -10,4 +11,5 @@ public interface RegisterTask {
      * @param resultListener ログインタスクの結果を受けて処理を実行するリスナー（画面）
      */
     void execute(EditableUserInfo userInfo, ResultListener resultListener);
+
 }
