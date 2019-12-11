c51f9b7d5339075811cd5752fb0dc0fc713e2a4b
LPNJ <Hiroto.Komiyama@jp.ricoh.com>
2019-12-11 12:21:44 +0900

    （どういう変更をしたか）deleteActivityの完成（MOCK）
    （なぜその変更をしたのか）省略

--- app/src/main/java/com/example/ojttask/EventSelectActivity.java
+++ app/src/main/java/com/example/ojttask/EventSelectActivity.java
@@ -12,14 +12,15 @@ import android.widget.ListView;
 
 import java.util.List;
 
+import Task.EventSelectTask;
 import Task.ResultListener;
 import Task.mock.EventSelectTaskMock;
 import entity.LoginUser;
 import result.EventSelectResult;
 
-public class EventSelectActivity extends AppCompatActivity implements ResultListener<EventSelectResult>, AdapterView.OnItemClickListener {
+public class EventSelectActivity extends AppCompatActivity implements ResultListener<EventSelectResult> {
 
-    private final EventSelectTaskMock mEventSelectTask;
+    private final EventSelectTask mEventSelectTask;
 
     /**
      * デフォルトコンストラクタ
@@ -43,7 +44,7 @@ public class EventSelectActivity extends AppCompatActivity implements ResultList
     }
 
     @Override
-    public void onResult(EventSelectResult result) {
+    public void onResult(final EventSelectResult result) {
         if (result == null) {
             throw new IllegalArgumentException("result is null");
         }
@@ -58,16 +59,27 @@ public class EventSelectActivity extends AppCompatActivity implements ResultList
                     new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,eventNameList );
 
             events.setAdapter(arrayAdapter);
-            events.setOnItemClickListener(this);
+            events.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    // TODO メンバ変数に設定したList＜EventInfo＞からpositionのEventInfoを取得する
+                    Intent intent = getIntent();
+                    // TODO 前画面の情報をメンバ変数に設定する
+                    int id_info = intent.getIntExtra("screen_info", 0);
+                    Log.i("Menu", id_info +  "");
+                    switch (id_info) {
+                        case R.id.delete: {
+                            Intent i = new Intent(EventSelectActivity.this, DeleteActivity.class);
+                            i.putExtra(IntentKey.REFERENCE_EVENT,  result.getEventInfoList().get((int) id));
+                            startActivity(i);
+                        }
+                        break;
+                    }
+                    // TODO 前画面の情報をもとに次の画面に遷移する
+                    // TODO EventInfoからJsonを取得してintentに設定する。
+
+                }
+            });
         }
-    }
-
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        // TODO メンバ変数に設定したList＜EventInfo＞からpositionのEventInfoを取得する
-//        LIstview lIstview = ()
-        // TODO 前画面の情報をもとに次の画面に遷移する
-        // TODO EventInfoからJsonを取得してintentに設定する。
-
     }
 }
