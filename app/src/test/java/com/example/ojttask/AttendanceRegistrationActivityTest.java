94346c2d98364c34db17da2917144d5d356c6e5a
ykimurah <Ryosuke.Kimura@jp.ricoh.com>
2019-12-17 13:25:26 +0900

    Testを追加

--- app/src/test/java/com/example/ojttask/AttendanceRegistrationActivityTest.java
+++ app/src/test/java/com/example/ojttask/AttendanceRegistrationActivityTest.java
@@ -8,13 +8,14 @@ import org.robolectric.shadows.ShadowLog;
 
 import Task.AttendanceRegistrationTask;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 /**
  * 出欠登録画面のテスト
  */
 public class AttendanceRegistrationActivityTest {
-    private AttendanceRegistrationActivity mActivity;
+    private AttendanceRegistrationActivity mActivity;//Activity名が長いので省略した
     private AttendanceRegistrationTask mTask;
 
     @Before
@@ -28,15 +29,32 @@ public class AttendanceRegistrationActivityTest {
     public void testdown(){
     }
 
+    /**
+     * onCreateのテスト
+     */
     @Test
-    public void onCreate(){
+    public void onCreateTest(){
         //setupでonCreateのテスト実施しているためOK
     }
 
+    /**
+     * onResultのエラー処理テスト
+     */
     @Test
     public void onResultTest_err(){
         mActivity.onResult(1);
-
+        //TODO なんかエラー処理されるらしいので追加されたらテスト修正
     }
+
+    /**
+     * onResultの画面遷移テスト
+     */
+    @Test
+    public void onResultTest_OK(){
+        mActivity.onResult(0);
+        String nextActivityName = TestUtils.getNextActivityName(mActivity);
+        assertThat(nextActivityName,is(MenuActivity.class.getName()));
+    }
+
 
 }
